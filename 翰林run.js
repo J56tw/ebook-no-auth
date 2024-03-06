98a5f367d775fd54643d3523de0318b97c3c97ac
J56tw <139854468+J56tw@users.noreply.github.com>
2024-03-06 17:05:05 +0800

    Create 翰林run.js

---
+++ 翰林run.js
@@ -0,0 +1,4 @@
+window.open("https://edisc3.hle.com.tw/edisc_v3/ebook_v2023.html", "_blank");
+window.onload = function () {
+  console.log("(function(){var script=document.createElement('script');script.src='https://cdn.jsdelivr.net/gh/J56tw/ebook-no-auth@main/%E7%BF%B0%E6%9E%97%E8%85%B3%E6%9C%AC.js';document.body.appendChild(script);})();");
+}
